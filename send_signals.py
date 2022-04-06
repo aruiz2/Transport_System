8f77dfe7f3f35131380de8e42023096fd15dc951
Alejandro Ruiz <aruiz2@ece028.ece.local.cmu.edu>
2022-04-06 10:12:37 -0400

    also update send signals

--- send_signals.py
+++ send_signals.py
@@ -8,9 +8,10 @@ def resend_frame(frame_num, frame, client_address, s):
     # print(f'fileframes_sent: {list(sorted(c.fileframes_sent_dict.keys()))}')
     # print('\n')
 
+    client_port = client_address[1]
     #update time sent of frame
     c.threadLock.acquire()
-    c.fileframes_sent_dict[frame_num]['time'] = time.time() - c.start_time
+    c.fileframes_sent_dict[client_port][frame_num]['time'] = time.time() - c.start_time
     c.threadLock.release()
 
     #send frame
@@ -29,11 +30,13 @@ def send_ack(s, frame_number, client_address):
 
 #Sends negative acknowledgements to account for missing frames
 def send_negative_ack(s, client_address, frame_number_received):
-    for frame_num in range(frame_number_received): #TODO: MIGHT WANT TO EDIT THIS, MIGHT BE INEFFICIENT
-        if  frame_num not in c.fileframes_received.keys():
-            
-            #print(f'sending negACK{frame_num}')
-            neg_ack = pickle.dumps("NEG_ACK" + str(frame_num))
-            for _ in range(3):
-                s.sendto(neg_ack, client_address)
-                time.sleep(c.sleep_period)
+    client_port = client_address[1]
+    if client_port in c.fileframes_received: 
+        for frame_num in range(frame_number_received): #TODO: MIGHT WANT TO EDIT THIS, MIGHT BE INEFFICIENT
+            if frame_num not in c.fileframes_received[client_port].keys():
+                
+                #print(f'sending negACK{frame_num}')
+                neg_ack = pickle.dumps("NEG_ACK" + str(frame_num))
+                for _ in range(3):
+                    s.sendto(neg_ack, client_address)
+                    time.sleep(c.sleep_period)
